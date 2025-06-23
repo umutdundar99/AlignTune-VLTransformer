abf1cbace9e8b3304651e1a9af9b2808fcc6fd18
umutdundar99 <umutdundar37@gmail.com>
2025-06-24 01:49:06 +0300

    Add CIDEr and METEOR metrics for training and validation; refactor metric logging

--- aligntune/src/module.py
+++ aligntune/src/module.py
@@ -1,23 +1,52 @@
 import torch
 import lightning as L
-from torchmetrics.text import ROUGEScore
+from torchmetrics.text import ROUGEScore, BLEUScore
 from pycocoevalcap.cider.cider import Cider
 from transformers import PaliGemmaForConditionalGeneration
-from nltk.translate.bleu_score import SmoothingFunction
 from aligntune.utils.processor import PaliGemmaProcessor
 import pandas as pd
+import torchmetrics as tm
+from torchmetrics import Metric
+from nltk.translate.meteor_score import meteor_score
 
 
-class CIDErWrapper:
+class CIDErMetric(Metric):
+    full_state_update = True  # accumulate all
+
     def __init__(self):
+        super().__init__(dist_sync_on_step=False)
+        self.add_state("preds", default=[], dist_reduce_fx="cat")
+        self.add_state("targets", default=[], dist_reduce_fx="cat")
         self.cider = Cider()
 
-    def __call__(self, predictions, references):
-        gts = {i: [ref] for i, ref in enumerate(references)}
-        res = {i: [pred] for i, pred in enumerate(predictions)}
+    def update(self, preds, targets):
+        self.preds.extend(preds)
+        self.targets.extend(targets)
+
+    def compute(self):
+        res = {i: [p] for i, p in enumerate(self.preds)}
+        gts = {i: [t] for i, t in enumerate(self.targets)}
         score, _ = self.cider.compute_score(gts, res)
         return score
+class METEORMetric(Metric):
+    full_state_update = True
 
+    def __init__(self):
+        super().__init__()
+        self.preds = []
+        self.targets = []
+
+    def update(self, preds, targets):
+        self.preds.extend(preds)
+        self.targets.extend(targets)
+
+    def compute(self):
+        scores = [meteor_score(refs, pred) for pred, refs in zip(self.preds, self.targets)]
+        return sum(scores) / len(scores)
+
+    def reset(self):
+        self.preds.clear()
+        self.targets.clear()
 
 class PaliGemmaModule(L.LightningModule):
     def __init__(
@@ -39,10 +68,25 @@ class PaliGemmaModule(L.LightningModule):
         self.max_tokens_to_generate = max_tokens_to_generate
         self.temperature = temperature
         self.top_p = top_p
-        self.rouge = ROUGEScore()
-        self.CIDEr = CIDErWrapper()
+  
+        self.training_metrics_rouge_cider = tm.MetricCollection({
+            "rouge": ROUGEScore(),
+           
+            "CIDEr": CIDErMetric(),    
+        })
+        self.validation_metrics_rouge_cider  = tm.MetricCollection({
+            "rouge": ROUGEScore(),
+            
+            "CIDEr": CIDErMetric(),
+        })
+        self.training_metric_bleu = tm.MetricCollection({
+            "bleu": BLEUScore(),
+        })
+        self.validation_metric_bleu = tm.MetricCollection({
+            "bleu": BLEUScore(),
+        })
 
-        self.smooth = SmoothingFunction().method1
+        
         self.save_hyperparameters(ignore=["model", "processor"])
         self.df = pd.DataFrame(columns=["generated", "actual"])
 
@@ -53,7 +97,7 @@ class PaliGemmaModule(L.LightningModule):
             "<image> <bos> describe this image." for _ in range(len(batch["image"]))
         ]
         inputs = self.processor(batch["image"], prompt, return_tensors="pt").to("cuda")
-        generated_tokens = self.model.generate(**inputs, max_new_tokens=20)
+        generated_tokens = self.model.generate(**inputs, max_new_tokens=self.max_tokens_to_generate)
         return outputs.loss, generated_tokens
 
     def training_step(self, batch, batch_idx):
@@ -67,22 +111,28 @@ class PaliGemmaModule(L.LightningModule):
             self.processor.tokenizer.decode(seq[seq != -100], skip_special_tokens=True)
             for seq in batch["labels"]
         ]
+        actual_captions_list = [[ref] for ref in actual_captions]
 
-        cider_score = self.CIDEr(generated_captions, actual_captions)
-        rouge_scores = self.rouge(generated_captions, actual_captions)
+        metrics_rouge_cider = self.training_metrics_rouge_cider(
+            generated_captions, actual_captions
+        )
+        metrics_bleu = self.training_metric_bleu(generated_captions, actual_captions_list)
+
 
         self.log_dict(
             {
                 "train/loss": loss,
-                "train/cider": cider_score,
-                "train/rouge1": rouge_scores["rouge1_fmeasure"],
-                "train/rouge2": rouge_scores["rouge2_fmeasure"],
-                "train/rougeL": rouge_scores["rougeL_fmeasure"],
+                "train/rouge1": metrics_rouge_cider["rouge1_fmeasure"],
+                "train/rouge2": metrics_rouge_cider["rouge2_fmeasure"],
+                "train/rougeL": metrics_rouge_cider["rougeL_fmeasure"],
+                "train/bleu": metrics_bleu["bleu"],
+                "train/cider": metrics_rouge_cider["CIDEr"],
             },
             on_step=True,
             on_epoch=True,
             prog_bar=True,
         )
+        self.log("train/loss", loss, on_step=True, on_epoch=True, prog_bar=True)
 
         return loss
 
@@ -97,6 +147,9 @@ class PaliGemmaModule(L.LightningModule):
             for seq in batch["labels"]
         ]
 
+        actual_captions_list = [[ref] for ref in actual_captions]
+        
+
         self.df = pd.concat(
             [
                 self.df,
@@ -107,22 +160,12 @@ class PaliGemmaModule(L.LightningModule):
             ignore_index=True,
         )
 
-        cider_score = self.CIDEr(generated_captions, actual_captions)
-        rouge_scores = self.rouge(generated_captions, actual_captions)
-
-        self.log_dict(
-            {
-                "val/loss": loss,
-                "val/cider": cider_score,
-                "val/rouge1": rouge_scores["rouge1_fmeasure"],
-                "val/rouge2": rouge_scores["rouge2_fmeasure"],
-                "val/rougeL": rouge_scores["rougeL_fmeasure"],
-            },
-            on_step=True,
-            on_epoch=True,
-            prog_bar=True,
+        self.validation_metrics_rouge_cider.update(
+            generated_captions, actual_captions
         )
-
+        self.validation_metric_bleu.update(
+            generated_captions, actual_captions_list
+        )
         return loss
 
     def on_validation_epoch_end(self):
@@ -139,6 +182,30 @@ class PaliGemmaModule(L.LightningModule):
             ignore_index=True,
         )
         self.df.to_csv("generated_captions.csv", index=False)
+
+        # log validation metrics
+        metrics_rouge_cider = self.validation_metrics_rouge_cider.compute()
+        metrics_bleu = self.validation_metric_bleu.compute()
+        self.log_dict(
+            {
+                "val/rouge1": metrics_rouge_cider["rouge1_fmeasure"],
+                "val/rouge2": metrics_rouge_cider["rouge2_fmeasure"],
+                "val/rougeL": metrics_rouge_cider["rougeL_fmeasure"],
+                "val/bleu": metrics_bleu["bleu"],
+                "val/cider": metrics_rouge_cider["CIDEr"],
+            },
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+        )
+        
+        self.validation_metrics_rouge_cider.reset()
+        self.validation_metric_bleu.reset()
+
+    def on_train_epoch_end(self):
+        # reset train metrics
+        self.training_metrics_rouge_cider.reset()
+        self.training_metric_bleu.reset()
 
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(
